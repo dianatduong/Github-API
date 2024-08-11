9ce604f9a8a3f95806f573aab9385872d98b8acd
Diana Duong <dduong013@gmail.com>
2024-08-11 14:06:08 -0700

    Initial Commit

---
+++ APITest/APITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  APITestApp.swift
+//  APITest
+//
+//  Created by Diana Duong on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct APITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
